3ed66d7f4c2201fdce96cb2f09d44d975c01c498
afreenbegumk <42670676+afreenbegumk@users.noreply.github.com>
2019-02-07 15:02:38 +0530

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int num1;
+	printf("enter the integer")
+	Scanf("%d"&num1);
+	if (num1%2==0)
+	printf("%d,even"num1);
+	else if(num1%2!=0);
+	printf("%d,invalid",num1);
+	else
+	printf("%d,odd",num1);
+	return(0);
+}
